ea7e825f93f431bcf7fd8b3ce6b046876ab72e39
user name <email@domain.com>
2021-04-30 20:42:51 +0400

    working on expenses

--- HomePage.js
+++ HomePage.js
@@ -7,3 +7,36 @@ function sideBarShow(){
         document.querySelector('#sidebar').style.display = 'none'
         sideBar = false }  
 }
+
+
+function showValueBox(){
+    document.querySelector('#box-amount').style.display = ''
+}
+
+function closShowValueBox(){
+    document.querySelector('#box-amount').style.display = 'none'
+}
+
+
+let expenValue = 0
+
+
+function addValue(){
+    
+    expenValue = expenValue + Number(document.querySelector("#valueinput").value)
+    console.log(typeof(document.querySelector("#valueinput").value))
+    console.log(expenValue)
+    document.querySelector('#expenvalue').innerHTML = `- ${expenValue}`
+    document.querySelector('#box-amount').style.display = 'none'
+
+
+
+    
+
+
+
+
+
+
+
+}
